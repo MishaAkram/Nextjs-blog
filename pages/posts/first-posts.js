6f2fd49cfbfbe31c0473bc74dabdeac1f3d5bbf6
MishaAkram <mishaakrambaig@gmail.com>
2021-07-06 00:32:15 +0500

    added layout components to pages

--- pages/posts/first-posts.js
+++ pages/posts/first-posts.js
@@ -1,14 +1,25 @@
 import Link from 'next/link'
-
+import Image from 'next/image'
+import Head from 'next/head'
+import Layout from '../../components/Layout';
 export default function FirstPost() {
-    return( 
-    <>
-    <h1>First Post</h1>
-    <h2>
-        <Link href="/">
-            <a>Back to home</a>
-        </Link>
-    </h2>
-    </>
+    return (
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <h1>First Post</h1>
+            <Image
+                src="/images/profile.jpg"
+                height="400"
+                width="400"
+
+                alt="Your Name" />
+            <h2>
+                <Link href="/">
+                    <a>Back to home</a>
+                </Link>
+            </h2>
+        </Layout>
     )
-  }
+}
