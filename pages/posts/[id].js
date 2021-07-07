e7784f621282b0e9741cf1e032c03d325010a8f0
MishaAkram <mishaakrambaig@gmail.com>
2021-07-07 21:57:11 +0500

    render markdown data

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,7 +2,7 @@ import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData =await getPostData(params.id)
   return {
     props: {
       postData
@@ -17,12 +17,16 @@ export async function getStaticPaths() {
     fallback: false
   }
 }
-export default function Post({postData}) {
-  return ( <Layout>
-    {postData.title}
-    <br />
-    {postData.id}
-    <br />
-    {postData.date}
-  </Layout>)
-}
+export default function Post({ postData }) {
+    return (
+      <Layout>
+        {postData.title}
+        <br />
+        {postData.id}
+        <br />
+        {postData.date}
+        <br />
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </Layout>
+    )
+  }
